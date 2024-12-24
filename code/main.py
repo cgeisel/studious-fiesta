4fd5c9a9fdea31656cd27f14b795bd46ed254453
Christopher Geisel <cgeisel@lululemon.com>
2024-12-24 08:26:49 -0800

    input

--- code/main.py
+++ code/main.py
@@ -19,7 +19,7 @@ y = 150
 # import an image
 player_surface = pygame.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surface.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
-player_direction = pygame.math.Vector2(1, 1)
+player_direction = pygame.math.Vector2()
 player_speed = 300
 
 star_surface = pygame.image.load(join('images', 'star.png')).convert_alpha()
@@ -34,10 +34,27 @@ laser_rect = laser_surf.get_frect(bottomleft = (WINDOW_WIDTH -1260, WINDOW_HEIGH
 while running:
     dt = clock.tick() / 1000
 
-    # events
+    # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # if event.type == pygame.KEYDOWN:
+        #     print(event.key == pygame.K_1)
+        # if event.type == pygame.MOUSEMOTION:
+        #     player_rect.center = event.pos
+
+    # input
+    # print(pygame.mouse.get_rel())
+    keys = pygame.key.get_pressed()
+    
+    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+
+    if pygame.key.get_just_pressed()[pygame.K_SPACE]:
+        print('fire laser')
+    
+    player_direction = player_direction.normalize() if player_direction else player_direction
+    player_rect.center += player_direction * player_speed * dt
 
     # draw
     # fill window with red color
@@ -48,11 +65,7 @@ while running:
     display_surface.blit(laser_surf, laser_rect)
     display_surface.blit(meteor_surf, meteor_rect)
 
-    player_rect.center += player_direction * player_speed * dt
-    if player_rect.bottom > WINDOW_HEIGHT or player_rect.top < 0:
-        player_direction.y *= -1
-    if player_rect.right > WINDOW_WIDTH or player_rect.left <0:
-        player_direction.x *= -1
+    
 
 
     display_surface.blit(player_surface, player_rect)
