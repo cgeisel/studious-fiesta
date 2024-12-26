6f3d55ab0a8fd879cfd50c9d1d0052052c3ff56b
Christopher Geisel <cgeisel@lululemon.com>
2024-12-26 07:18:44 -0800

    adding font, borders, fake score

--- code/main.py
+++ code/main.py
@@ -36,6 +36,12 @@ class Player(pygame.sprite.Sprite):
             self.can_shoot = False
             self.laser_shoot_time = pygame.time.get_ticks()
 
+        global running
+        collision_sprites =  pygame.sprite.spritecollide(self, meteor_sprites, False)
+        if collision_sprites:
+            self.kill()
+            running = False
+
         self.laser_timer()
 
 
@@ -78,6 +84,13 @@ class Meteor(pygame.sprite.Sprite):
             self.kill()
 
 
+def display_score():
+    current_time = pygame.time.get_ticks()
+    text_surf = font.render(str(current_time), True, 'white')
+    text_rect = text_surf.get_frect(midbottom = (WINDOW_WIDTH / 2, WINDOW_HEIGHT - 50))
+    display_surface.blit(text_surf, text_rect)
+    pygame.draw.rect(display_surface, 'white', text_rect.inflate(20, 16).move(0, -8), 5, 10)
+
 # general setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
@@ -90,6 +103,7 @@ clock = pygame.time.Clock()
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
 laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
+font = pygame.font.Font(join('images', 'Oxanium-Bold.ttf'), 40)
 
 # sprites
 all_sprites = pygame.sprite.Group()
@@ -122,10 +136,10 @@ while running:
     all_sprites.update(dt)
     
     # draw
-    display_surface.fill('darkgray')
+    display_surface.fill('#3a2e3f')
     all_sprites.draw(display_surface)
     
-
+    display_score()
 
     pygame.display.update()
 
